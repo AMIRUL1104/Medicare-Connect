a3ca665fbda0bdd21e9773846c6541b94fc4301a
Amirul <amirulislam9.e@gmail.com>
2026-06-18 15:25:35 +0600

    basic  setup complete

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import { navlinks } from "../shared/Navlinks";
+
+function Navbar() {
+  const navlink = navlinks;
+  // console.log(navlink);
+
+  return <div>Navbar</div>;
+}
+
+export default Navbar;
---
+++ src/components/shared/Navlinks.js
@@ -0,0 +1,5 @@
+export const navlinks = [
+  { label: "Home", icon: "", href: "/" },
+  { label: "Find Doctors", icon: "", href: "/doctors" },
+  { label: "Signin", icon: "", href: "/auth/signin" },
+];
